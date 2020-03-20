fe15b630245892b0d795fb268e506079241c1258
Diana-Victores <dcpernillo@gmail.com>
2020-03-19 20:12:49 -0600

    sede

--- Diana Victores/proyect_sede.sql
+++ Diana Victores/proyect_sede.sql
@@ -1,5 +1,32 @@
-Use sede;
+create table sede;
+use UMG;
+create table edificio
+(
+    Codigosede varchar (5) PRIMARY KEY,
+    Nombrecarreras varchar (50) NOT null,
+    Estatusedificio varchar (1) NOT null
+    )engine=INNODB;
 
-Insert into edificioo (Codigosede,Nombrecarreras,Estatusedificio) values('1','Ingenieria','a');
+create table carreras
+(
+Codigocarreras varchar (5) primary key,
+Nombrecarerras varchar (50) not null,
+Estatuscarrera varchar (1) not null
+)engine=innodb;
 
-Select * from edificioo;	
+create table muebless
+(
+Codigomobiliario varchar (5) primary key,
+Nombremueble varchar (50) not null,
+Codigocarrera varchar (5) not null,
+Codigomuebles varchar (5) not null,
+EstatusEmpleado varchar (1) not null,
+Codigosede varchar(5) not null,
+
+foreign key (Codigosede) references edificio (Codigosede),
+foreign key (Codigoedificio) references carreras (Codigoedificio)
+
+
+)engine=innodb;
+
+
