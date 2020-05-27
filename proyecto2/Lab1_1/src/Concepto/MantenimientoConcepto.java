1cda0c36153f7ee1faa508f0da6f46419651f968
JackyCarrera <jackycarrera1234@gmail.com>
2020-05-27 15:28:38 -0600

    Se finalizo el mantenimiento concepto Jaqueline

--- proyecto2/Lab1_1/src/Concepto/MantenimientoConcepto.java
+++ proyecto2/Lab1_1/src/Concepto/MantenimientoConcepto.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package Concepto;
-
+import Recibo.recibo;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Jacky
